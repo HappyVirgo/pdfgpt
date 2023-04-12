c47abf5fdf4db34179419ebbddc0c25e82950327
mern-dev-pro <koalah86@gmail.com>
2023-04-12 11:39:13 +0000

    add: chatting history api

---
+++ pages/api/history/save-chatting.ts
@@ -0,0 +1,16 @@
+import axios from "axios";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const { body, token } = req.body;
+  try {
+    const { data } = await axios.post(`${process.env.BACKEND_API_BASEURL}/history/chat`, body, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    res.status(200).json({ data });
+  } catch (error: any) {
+    return res.status(500).json(error?.response?.data);
+  }
+}
